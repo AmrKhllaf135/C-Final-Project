bb0954561cb14d59566a19bef7b32abb5c74ecc9
Mahmoud Hamdy <50111950+MahmoudHamdyMahmoud@users.noreply.github.com>
2021-08-24 17:55:29 +0200

    Upper function

--- C-Final-Project.c
+++ C-Final-Project.c
@@ -1,6 +1,33 @@
 int key =3;
 int length;
 char s[26];
+
+void Encryption_Upper(char s[26])
+{
+	int i;
+	length = strlen(s);
+    for(i=0 ;i <length; i++)
+    {
+        if(s[i] + key <= 'Z')
+            s[i] += key;
+       else
+            s[i] = 'A' + ((s[i] + key) - 'Z' -1);
+    }
+    printf("%s\n", s);
+}
+void Decryption_Upper(char s[26])
+{
+	int i;
+	length = strlen(s);
+    for(i=0 ;i <length; i++)
+    {
+        if(s[i] - key >= 'A')
+            s[i] -= key;
+       else
+            s[i] = ('Z' -key) + (s[i] - 'A') +1 ;
+    }
+    printf("%s\n", s);
+}
 void Encryption_Lower(char s[26])
 {
 	int i;
