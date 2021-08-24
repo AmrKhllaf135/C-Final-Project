beba3675429d1d83c8b08cdcae876ec08381570c
AmrKhllaf135 <amrkhllaf135@gmail.com>
2021-08-24 18:04:06 +0200

    Update C-Final-Project.c

--- C-Final-Project.c
+++ C-Final-Project.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int key =3;
 int length;
 char s[26];
@@ -54,3 +58,49 @@ void Decryption_Lower(char s[26])
     }
     printf("%s\n", s);
 }
+
+int main()
+{
+	 while(1)
+    {
+        printf("Press 1 to Encrypt for Upper String\n");  
+        printf("Press 2 to Decrypt for Upper String\n");
+		 printf("Press 3 to Encrypt for Lower String\n");  
+        printf("Press 4 to Decrypt for Lower String\n"); 
+        printf("Press 5 to exit\n");
+        int input;
+		scanf("%d" , &input);
+        printf("Please Enter a string\n"); 
+        char s[26];
+        scanf("%s" ,&s); 
+        switch(input)
+        {
+        case 1 :
+            printf("String for Upper Encryption is: ");
+            Encryption_Upper(s);
+            break;
+        case 2:
+            printf("String for Upper Decryption is: ");
+            Decryption_Upper(s);
+            break;
+        case 3 :
+            printf("String for Lower Encryption is: ");
+            Encryption_Lower(s);
+            break;
+        case 4:
+            printf("String for Lower Decryption is: ");
+            Decryption_Lower(s);
+            break;    
+        case 5 :
+            return 0;
+            break;
+        default:
+            printf("Wrong choice\n");
+            break;
+        }
+    }
+
+
+    return 0;
+}
+	
